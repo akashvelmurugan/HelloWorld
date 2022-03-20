bcc80216c2ecde7400586317f25b1474de882e3c
akashvelmurugan <akashvelmurugan125@gmail.com>
2022-03-20 23:34:49 +0530

    HelloWorld

---
+++ src/com/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.helloworld;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+
+		String string="Hello World  is is";
+
+		String[] split = string.split(" ");
+		String stringCompare="1";
+
+
+		
+	}
+}
